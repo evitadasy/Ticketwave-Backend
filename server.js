8cb5f0845243f70838be9a3b3d63d09fbaf3349e
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-11-22 19:16:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 require('dotenv').config()
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
