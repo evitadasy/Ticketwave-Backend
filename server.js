6b7cb58fa5c737c599a5dc48f55723296839390c
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-11-22 19:12:17 +0200

    Revert "Update server.js"
    This reverts commit 97b1e5889142df4aebab88df88e68c516ada3958.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 require('dotenv').config()
 
-const port = 3000;
+const port = process.env.PORT;
 const server = http.createServer(app);
 
 server.listen(port);
